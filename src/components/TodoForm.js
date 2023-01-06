46a44eb75af915cc0e32c213f70132effb4b069d
chetansource <chetankumargopi149@gmail.com>
2023-01-06 10:04:29 +0530

    fixed delete functionality its deleting from localstorage

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,50 +4,55 @@ import './TodoForm.css'
 import TodoItem from './TodoItem.js'
 
 function TodoForm() {
-    const [inputText, setInputText] = useState('')
-    const [todos, setTodos] = useState(
-        JSON.parse(localStorage.getItem('items')) || []
-    )
+  const [inputText, setInputText] = useState('')
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  )
 
-    function addTodo(event) {
-        event.preventDefault()
-        console.log(inputText)
-        if (inputText.trim() === '') return
-        todos.push({
-            id:
-                todos.length === 0
-                    ? 0
-                    : Math.max(...todos.map((e) => e.id)) + 1,
-            title: inputText,
-            checkbox: false,
-            dueDate: '',
-            notes: '',
-            priority: ''
-        })
-        localStorage.setItem('items', JSON.stringify(todos))
-        setInputText('')
-    }
+  function addTodo(event) {
+    event.preventDefault()
+    if (inputText.trim() === '') return
+    todos.push({
+      id: todos.length === 0 ? 0 : Math.max(...todos.map((e) => e.id)) + 1,
+      title: inputText,
+      checkbox: false,
+      dueDate: '',
+      notes: '',
+      priority: ''
+    })
+    localStorage.setItem('items', JSON.stringify(todos))
+    setInputText('')
+  }
+  function deleteTodo(id) {
+    const removeTodo = todos.filter((todo) => {
+      return todo.id !== id
+    })
+    setTodos(removeTodo)
+    localStorage.setItem('items', JSON.stringify(removeTodo))
+  }
 
-    return (
-        <form>
-            <input
-                className='title'
-                type='text'
-                name='inputText'
-                placeholder='what do you want to achieve...?'
-                value={inputText}
-                onChange={(event) => setInputText(event.target.value)}
-            />
-            <button className='submitBtn' onClick={addTodo}>
-                ADD TASK
-            </button>
-            <div>
-                {todos.map((todo) => (
-                    <TodoItem key={todo.id} todo={todo} />
-                ))}
-            </div>
-        </form>
-    )
+  return (
+    <form className='todoForm'>
+      <div className='form'>
+        <input
+          className='title'
+          type='text'
+          name='inputText'
+          placeholder='what do you want to achieve...?'
+          value={inputText}
+          onChange={(event) => setInputText(event.target.value)}
+        />
+        <button className='submitBtn' onClick={addTodo}>
+          ADD TASK
+        </button>
+      </div>
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} onDelete={deleteTodo} />
+        ))}
+      </div>
+    </form>
+  )
 }
 
 export default TodoForm
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,75 +1,65 @@
 import React, { useState } from 'react'
 import './TodoItem.css'
 function TodoItem(props) {
-    // const [checkbox, setCheckBox] = useState(false)
-    // function updateCheckBox() {
-    //     let tempTodos = JSON.parse(localStorage.getItem('items'))
+  console.log(props.onDelete)
+  const optionList = ['none', 'low', 'medium', 'high']
+  const [priority, setPriority] = useState('none')
+  function handlePriority(e) {
+    setPriority(e.target.value)
+  }
 
-    //     localStorage.setItem('items', JSON.stringify(tempTodos))
-    // }
+  const [dropDown, setDropDown] = useState(false)
+  function handleClick() {
+    setDropDown(!dropDown)
+  }
 
-    function deleteTodo(id) {
-        // let tempTodos = JSON.parse(localStorage.getItem('items'))
-        // tempTodos = tempTodos.filter((todo) => todo.id !== id)
-        // localStorage.setItem('items', JSON.stringify(tempTodos))
-    }
-
-    const optionList = ['none', 'low', 'medium', 'high']
-    const [priority, setPriority] = useState('none')
-    function handlePriority(e) {
-        setPriority(e.target.value)
-    }
-
-    const [dropDown, setDropDown] = useState(false)
-    function handleClick() {
-        setDropDown(!dropDown)
-    }
-
-    return (
-        <div>
-            <input
-                type='checkbox'
-                className='checkBox'
-                value={props.todo.checkbox}
-            ></input>
-
-            <input
-                type='text'
-                className='todoTitle'
-                value={props.todo.title}
-                onClick={handleClick}
-            ></input>
-            <div>
-                {dropDown && (
-                    <div>
-                        <textarea
-                            className='textNote'
-                            value={props.todo.note}
-                        ></textarea>
-                        <label for='dateInput'>DueDate:</label>
-                        <input
-                            type='date'
-                            id='dueDate'
-                            value={props.todo.dueDate}
-                        ></input>
-                        <label for='primacy'>Priority:</label>
-                        <select value={priority} onChange={handlePriority}>
-                            {optionList.map((item) => {
-                                return <option>{item}</option>
-                            })}
-                        </select>
-                        <div>
-                            <button
-                                className='delTodo'
-                                onClick={deleteTodo(props.todo.id)}
-                            >
-                                Delete
-                            </button>
-                        </div>
-                    </div>
-                )}
+  return (
+    <div>
+      <div className='addTodo'>
+        <input
+          type='checkbox'
+          className='checkBox'
+          value={props.todo.checkbox}
+        ></input>
+        <input
+          type='text'
+          className='todoTitle'
+          value={props.todo.title}
+          onClick={handleClick}
+        ></input>
+      </div>
+      <div className='dropdown'>
+        {dropDown && (
+          <div>
+            <textarea className='textNote' value={props.todo.note}></textarea>
+            <div className='elements'>
+              <div>
+                <label for='dateInput'>DueDate:</label>
+                <input
+                  type='date'
+                  id='dueDate'
+                  value={props.todo.dueDate}
+                ></input>
+              </div>
+              <div>
+                <label for='primacy'>Priority:</label>
+                <select value={priority} onChange={handlePriority}>
+                  {optionList.map((item) => {
+                    return <option>{item}</option>
+                  })}
+                </select>
+              </div>
+              <button
+                className='delTodo'
+                onClick={() => props.onDelete(props.todo.id)}
+              >
+                Delete
+              </button>
             </div>
-        </div>
-    )
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 export default TodoItem
