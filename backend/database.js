3b5672d39d3ebb3505fa4b227a618d839a6f3d6d
chetansource <chetankumargopi149@gmail.com>
2023-01-10 19:02:15 +0530

    server connection is fixed, but when i execute query server connection is failing

--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,26 @@
 import { createClient } from 'redis'
-const redisPort = 6380
-const client = createClient(redisPort)
+const client = createClient({
+  url: 'redis://@localhost:6380'
+})
 
 export async function connectDataBase() {
   try {
-    client.on('error', (err) => console.log('Redis Client Error', err))
     await client.connect()
-    console.log('connected..')
+    console.log(' Redis Client connected..')
   } catch (error) {
-    console.log('Error', error)
+    console.log('Redis Client Error', error)
+    throw Error
   }
 }
+
+export async function getTodos() {
+  try {
+    const data = await client.hGet('name')
+    return data
+  } catch (error) {
+    console.log('database error', error)
+    throw Error
+  }
+}
+
+// console.log(await getTodos())
