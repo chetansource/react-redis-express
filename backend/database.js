88f30a522d22f7aefc48d858bf258ee6aef41a6f
chetansource <chetankumargopi149@gmail.com>
2023-01-11 14:17:32 +0530

    fixed the socket hang up error , insert and fetch todo are working

--- backend/database.js
+++ backend/database.js
@@ -15,12 +15,37 @@ export async function connectDataBase() {
 
 export async function getTodos() {
   try {
-    const data = await client.hGet('name')
-    return data
+    const data = await client.hGetAll('todos')
+    const todos = Object.values(data)
+    return todos.map((todo) => JSON.parse(todo))
+  } catch (error) {
+    console.log('db error:', error)
+    throw Error
+  }
+}
+export async function insertTodo(todo) {
+  try {
+    const newTodo = {
+      title: todo,
+      checkbox: '',
+      notes: '',
+      dueDate: '',
+      priority: ''
+    }
+    await client.incr('counter')
+    const newId = await client.get('counter')
+    return await client.hSet('todos', newId, JSON.stringify(newTodo))
   } catch (error) {
     console.log('database error', error)
     throw Error
   }
 }
 
-// console.log(await getTodos())
+export async function delTodo(id) {
+  try {
+    return await client.hDel('todos', id)
+  } catch (error) {
+    console.log('database error', error)
+    throw Error
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,42 @@
 import express from 'express'
-import { connectDataBase } from './database.js'
+import { connectDataBase, getTodos, insertTodo, delTodo } from './database.js'
 
 const app = express()
 connectDataBase()
+
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  try {
+    const todos = await getTodos()
+    console.log(todos)
+    res.json(todos)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+app.post('/hSet', async (req, res) => {
+  try {
+    const addTodo = await insertTodo(req.body.title)
+    res.json(addTodo)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
+})
+
+app.delete('/hDel/id', async (req, res) => {
+  try {
+    await delTodo(req.params.id)
+    res.sendStatus(204)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+app.listen(3000, () => {
+  console.log('welcome to redis')
+})
